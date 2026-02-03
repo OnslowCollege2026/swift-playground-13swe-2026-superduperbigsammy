874d42ebfdcc3101b6e100333190fe2d2da261f0
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-02-03 23:55:55 +0000

    Started the revision! Wow! :D

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,7 +3,28 @@
 
 @main
 struct SwiftPlayground {
+let budget:[Double] = 35.00
+let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
+
+
     static func main() {
-        print("Hello, world!")
+        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
+        var day_counter:Int = 1
+        for costs in lunches{
+            print("Day \(day_counter): $\(costs)")
+            day_counter += 1
+        }
     }
+
+    /// Total Cost Function
+    /// 
+    /// - Returns the total price of all lunches
+    func totalCost(prices: [Double]) -> Double{
+        var totalPrice:Double = 0
+        for itemCosts in lunches{
+            totalPrice = totalPrice + itemCosts
+        }
+        return totalPrice
+    }
+    
 }
