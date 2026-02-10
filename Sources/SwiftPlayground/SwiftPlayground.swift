a0f14d859d721b451739da5dfcb7dbda2e71e807
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-02-10 23:56:01 +0000

    map filter reduce task 1 semi done

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,29 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
-    }
+        let numbers = [1, 2, 3, 4, 5]
+
+
+        let total = numbers.map { number in
+            return number * number * number
+        }.filter { number in
+            return number % 2 == 0
+        }.reduce(0) { result, number in
+            return result + number
+        }
+        print(total)
+
+
+        // task Nalyzing setudent scorews
+        let scores = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
+
+        let finalScores = scores.map { score in
+        return score + 5}
+        .filter { score in
+        return score >= 50 }
+        .reduce(0) {average, score in
+        return average + score}
+
+        print(finalScores / scores.count)
+    }   
 }
