7c6d36dded76a17a365558b4bc4605af9dd7e29c
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-02-16 01:59:30 +0000

    Sam Harford - Map, Filter, Reduce

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,29 +4,52 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let numbers = [1, 2, 3, 4, 5]
+        
+        // Task 1: Numbers
+        let numbers:[Int] = [7, 14, 21, 28, 35]
 
-
-        let total = numbers.map { number in
-            return number * number * number
-        }.filter { number in
-            return number % 2 == 0
-        }.reduce(0) { result, number in
-            return result + number
+        // Add all the numbers together
+        let total = numbers.reduce(0) { result, numberInString in
+            return result + numberInString
         }
         print(total)
 
 
-        // task Nalyzing setudent scorews
+
+        // Task 2: Strings
+        // The array with all the possible words
+        let words:[String] = ["apple", "banana", "grape", "strawberry", "kiwi"]
+        
+        // Finds the longest word. longest = current result, word = current point in the string
+        let longestWord = words.reduce("") {
+            longest, word in
+            // if the selected string is longer than current result, return selected string
+            if word.count > longest.count {
+                return word
+            }
+            else{ return longest}
+        }
+
+        // prints resu.t
+        print(longestWord)
+
+
+        // task: analyzing setudent scorews
         let scores = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
 
         let finalScores = scores.map { score in
-        return score + 5}
-        .filter { score in
-        return score >= 50 }
-        .reduce(0) {average, score in
-        return average + score}
+        // Adds 5 to every score
+        return score + 5
+        }
+        .filter {score in
+        // Only keeps scores >= 50
+        return score > 50}
+        .reduce(0){score, number in
+        // Adds all the scores together
+        return score + number
+        }
 
-        print(finalScores / scores.count)
-    }   
+
+        print(finalScores/scores.count)
+    }
 }
