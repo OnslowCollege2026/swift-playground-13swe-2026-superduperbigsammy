af8c4fa42a7bae04ad7818b21a0e61a348c047e9
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-02-23 02:08:35 +0000

    started mehtods

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,25 +6,29 @@
             let author: String
             let pages: Int
 
-            func bookSummary() -> String {
+            func summary() -> String {
                     return """
                     \(title) was written by \(author) and contains \(pages) pages.
                     """
             }
 
+
+        func bookSummary(for: Book,) -> String {
+            return "\(title) was written by \(author) and contains \(pages) pages."
+        }
 @main
 struct SwiftPlayground {
     static func main() {
 
-        bookSummary(title:author:pages:) -> String {
-            print("\(Book.title) was written by \(Book.author) and contains \(Book.pages) pages.")
-        }
 
 
         let book1OpenFunc = Book(title: "Book 1 open func", author: "John Bookwrite", pages:67)
-        bookSummary(book1OpenFunc)
+        print(bookSummary(book1OpenFunc))
+
         let book2Method = Book(title: "Book 2 Method", author: "John Bookwrite", pages:69)
-        book1OpenFunc.bookSummary()
+        print(book2Method.summary())
+
+
         }
 
     }
