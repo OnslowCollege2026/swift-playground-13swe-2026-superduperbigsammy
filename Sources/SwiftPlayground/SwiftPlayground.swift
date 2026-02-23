6052870636027dbebb3c24b675728e2d1ebbbcdf
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-02-23 01:52:35 +0000

    started mehtods task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,33 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+        struct Book {
+            let title: String
+            let author: String
+            let pages: Int
+
+            func bookSummary() -> String {
+                    return """
+                    \(title) was written by \(author) and contains \(pages) pages.
+                    """
+            }
+
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+
+        bookSummary(title:author:pages:) -> String {
+            print("\(Book.title) was written by \(Book.author) and contains \(Book.pages) pages.")
+        }
+
+
+        let book1OpenFunc = Book(title: "Book 1 open func", author: "John Bookwrite", pages:67)
+        bookSummary(book1OpenFunc)
+        let book2Method = Book(title: "Book 2 Method", author: "John Bookwrite", pages:69)
+        book1OpenFunc.bookSummary()
+        }
+
     }
 }
+
+//tung tung tung 
