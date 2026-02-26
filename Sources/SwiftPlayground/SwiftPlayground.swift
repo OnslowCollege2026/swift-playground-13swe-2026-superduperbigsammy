d343fa34bb56cc615cf98a3c10ad6fc457aaac68
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-02-26 22:04:20 +0000

    Started protocols

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,35 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+struct Student: Identifiable, Codable {
+    let id: Int
+    var name: String
+    var age: Int
+}
+struct Course: CustomStringConvertible, Codable {
+    let id = Int()
+    let title = String()
+    let courseDescription: String
+
+    var description: String { "Course \(title) (\(id)):\n\(courseDescription)"}
+}
+
+struct Enrolment: Codable {
+    let studentId: Int
+    let courseId: Int
+}
 
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let archieee = Student(id: 12, name: "Archie D", age: 17)
+        print(archieee)
+let enrolledStudent = Enrolment(studentId: 12, courseId: 16)
+print(enrolledStudent)
+let encodedStudent = try! JSONEncoder().encode(enrolledStudent)
+print(encodedStudent)
+let decodedStudent = try! JSONDecoder().decode(Student.self, from: encodedStudent)
+print(decodedStudent)
     }
 }
