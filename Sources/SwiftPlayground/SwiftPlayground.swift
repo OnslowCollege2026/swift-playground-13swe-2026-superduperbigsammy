39b27f9ff2d6a8a7a93274b83d25f6ca8c2fdc74
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-04-20 01:55:35 +0000

    enbd of term push

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,24 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let list = [3, 7, 8, 18]
+
+        let addedAll = list.reduce(0) { $0 + $1 }  // equals 36
+        print(addedAll)
+
+        let oddNum = list.filter { $0 % 2 != 0 }
+        print(oddNum)
+
+        let highestNum = list.reduce(0){Swift.max($0,$1)}
+        print(highestNum)
+
+        let under15 = list.filter{$0 - 15 <= 0}
+        print(under15)
+
+        let nearestTen = list.map {$0 + ($0-10)}
+        print(nearestTen)
+
     }
+
 }
+
