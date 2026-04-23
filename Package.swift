16b4707d72958f611f77239b970e71851f5d8e63
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-04-23 03:05:44 +0000

    Added book struct and correctly installed GRDB

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPlayground",
-
+    dependencies: [
+        .package(url: "https://github.com/groue/GRDB.swift", exact: "7.10.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -12,14 +12,12 @@ import GRDB
 // The current date when running the program - PUT INSIDE THE CODE LATER - HERE TEMP
 let currentDate = Date()
 
-
 // Structs
 
 /// A Customer is the registered client who is renting out a book from the system.
-struct Customer: Identifiable, Codable, CustomStringConvertible, FetchableRecord, PersistableRecord
-{
+struct Customer: Identifiable, Codable, FetchableRecord, PersistableRecord {
     // A Customer's unique identifier
-    let id: Int
+    let id: Int64
 
     // A Customer's name
     let name: String
@@ -37,14 +35,44 @@ struct Customer: Identifiable, Codable, CustomStringConvertible, FetchableRecord
         case phoneNumber = "Phone"
         case email = "Email"
     }
+}
 
+/// A Book is an instance of a book that is inside the library.
+struct Books: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    // A Book's unique identifier.
+    let id: Int64
 
+    // The title of the book.
+    let title: String
+
+    // The author of the book.
+    let author: String
+
+    // The total amount of copies that the library has in it's collection.
+    let totalCopies: Int
+
+    // Syncs the names between swift variables and titles in the database
+    enum CodingKeys: String, CodingKey {
+        case id = "bookId"
+        case title = "Title"
+        case author = "Author"
+        case totalCopies = "TotalCopies"
+    }
 }
 
 @main
 struct SwiftPlayground {
     static func main() {
-                let dbPath = "./library.db"
+
+        print("""
+        Welcome to the Onslow College Library admin panel. Please select an operation.
+        1. Rent a book
+        2. Return a book
+        3. Edit a database
+        4. Generate report
+        """)
+
+        let dbPath = "./library.db"
         guard let dbQueue = try? DatabaseQueue(path: dbPath) else {
             fatalError("Failed to connect to database.")
         }
