0eacdda0798ed848a89e7e89f8363dd5398954fe
superduperbigsammy <159368536+superduperbigsammy@users.noreply.github.com>
2026-04-23 23:14:42 +0000

    Added main menus and began creating functions

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -8,14 +8,23 @@ import Foundation
 import GRDB
 
 // Constants
+let dbPath = "./library.db"
 
-// The current date when running the program - PUT INSIDE THE CODE LATER - HERE TEMP
-let currentDate = Date()
+// This header goes at the top of each menu page.
+let header: String = """
+    ----------------------------------------------
+    \(Date()) - ONSLOW COLLEGE LIBRARY - ADMIN PANEL -
+    """
+
+// The buttons for the menus, used to make selections. Edit here for global change of menuButtons
+let menuButtons =
+    ["1", "2", "3", "4"]
 
 // Structs
 
 /// A Customer is the registered client who is renting out a book from the system.
-struct Customer: Identifiable, Codable, FetchableRecord, PersistableRecord {
+struct Customer: Identifiable, Codable, CustomStringConvertible, FetchableRecord, PersistableRecord
+{
     // A Customer's unique identifier
     let id: Int64
 
@@ -23,7 +32,7 @@ struct Customer: Identifiable, Codable, FetchableRecord, PersistableRecord {
     let name: String
 
     // A Customer's phone number
-    let phoneNumber: String
+    let phone: String
 
     // A Customer's email address (Optional)
     let email: String
@@ -32,8 +41,14 @@ struct Customer: Identifiable, Codable, FetchableRecord, PersistableRecord {
     enum CodingKeys: String, CodingKey {
         case id = "CustomerId"
         case name = "Name"
-        case phoneNumber = "Phone"
+        case phone = "Phone"
         case email = "Email"
+    }
+
+    var description: String {
+        return """
+            User \(id) - \(name). Contact: \(email) \(phone)
+            """
     }
 }
 
@@ -58,24 +73,88 @@ struct Books: Identifiable, Codable, FetchableRecord, PersistableRecord {
         case author = "Author"
         case totalCopies = "TotalCopies"
     }
+    var description: String {
+        return """
+            Book \(id) - \(title) - \(author). Library holds \(totalCopies) copies.
+            """
+    }
 }
 
 @main
 struct SwiftPlayground {
+
     static func main() {
 
-        print("""
-        Welcome to the Onslow College Library admin panel. Please select an operation.
-        1. Rent a book
-        2. Return a book
-        3. Edit a database
-        4. Generate report
-        """)
+        var isProgramRunning: Bool = true
 
-        let dbPath = "./library.db"
-        guard let dbQueue = try? DatabaseQueue(path: dbPath) else {
-            fatalError("Failed to connect to database.")
+        /// rentBook()
+        ///
+        /// *This function is used when the admin is renting out a book on behalf of the customer.*
+        func rentBook() {
+            // Displays a menu
+            print(
+                """
+                \(header) RENT A BOOK
+                Please select an operation.
+                \(menuButtons[0]). Register a customer (Required)
+                \(menuButtons[1]). Rent a book
+                \(menuButtons[2]). Cancel Operation
+                """)
         }
-        print(currentDate)
+
+        /// returnBook()
+        ///
+        /// *This function is used when the customer wants to return a book.*
+        // PUT HERE
+
+        /// editData()
+        ///
+        /// *This function is used when the customer wants to return a book.*
+        // PUT HERE
+
+        /// viewData()
+        ///
+        /// *This function is used when the customer wants to return a book.*
+        // PUT HERE
+
+        do {
+
+            // Stores the database queue, which is where the list of operations go.
+            let dbQueue = try DatabaseQueue(path: dbPath)
+            print("Connected to database.")
+
+            // Dump the schema to ensure we are connected to the correct database file.
+            try dbQueue.read({ database in
+                try database.dumpSchema()
+            })
+
+            while isProgramRunning == true {
+
+                print(
+                    """
+                    \(header) MAIN MENU
+                    Welcome to the Onslow College Library admin panel. Please select an operation.
+                    \(menuButtons[0]). Rent a book
+                    \(menuButtons[1]). Return a book
+                    \(menuButtons[2]). Edit a database
+                    \(menuButtons[3]). View a database
+                    """)
+                var userInput = readLine()
+                if userInput == menuButtons[0] {
+                    rentBook()
+                    break
+
+                } else if userInput == menuButtons[1] {
+
+                } else if userInput == menuButtons[2] {
+
+                } else {
+                    print("Please select an operation by typing \(menuButtons)")
+                }
+
+            }
+        } catch {
+            print(error)
+        }
     }
 }
